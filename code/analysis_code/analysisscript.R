82c9da6d456482362ec389435d6994225bb32acf
Carter Coleman <cac64024@uga.edu>
2021-11-17 23:53:56 -0500

    miscellaneous_edits

--- code/analysis_code/analysisscript.R
+++
@@ -1,8 +0,0 @@
-###############################
-# analysis script
-#
-#this script loads the processed, cleaned data, does complex analysis
-#for Carter Coleman's UOWN indicator of Stream Health Project.
-
-
-  
